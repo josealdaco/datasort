8b0c8bc36c2c4edc256846f5253f8374e3f280d2
Jose Arellanes <gamerbloxhd@gmail.com>
2021-12-13 14:33:03 -0800

    updating sortAssignment
    - Changed Name to make it clear that it will return the index and value based on the positional value provided
    - Using list comprehension to populate the list with only int types
    - Using built-in python function sort() to sort the list based off of the value of the tuple, this allows the collection of original indexes to remain unchanged

--- sortsecondaryLargest.py
+++ sortsecondaryLargest.py
@@ -1,55 +1,38 @@
 from typing import Optional
 
 
-def sortsecondLargest(input: list) -> Optional[tuple]:
+def sortnLargest(input: list, position=1) -> Optional[tuple]:
     '''
-            params: input of  type list
-            - We Ensure our input is of class List
-            - We create a storage to keep the original Index O(1), this  means
-                if there are copies it will return the latest secondary
-                integer value from the list
-            -  Iterates through the list O(n) and checks to ensure its a type
-                Integer and compates whether its a max integer or secondary
-            - Insert index to index_storage if its an integer
-            - If  there is no integers found it will return None if no integer
-            are found, otherwise it will return tuple with (index,value)
-            - If there is NO second largest it will return None instead of
-            largest This solutions runs a time Complexity at about
-            O(n) * O(1) --  O(1) From python Dictonary lookup & retrieval
-    '''
-    try:
-        assert isinstance(input, list)
-        index_storage = dict()
-        max_value = None
-        second_value = None
-        for index in range(len(input)):
-            if isinstance(input[index], int):
-                index_storage[input[index]] = index
-                if max_value is None:
-                    max_value = input[index]
-                else:
-                    if input[index] > max_value:
-                        second_value = max_value
-                        max_value = input[index]
-                    elif second_value is None or input[index] > second_value:
-                        second_value = input[index]
+        Params: [input] list that will contain many different data types ex.int
+                , String, float.
+                [position] Which position to retrieve from the input data list,
+                ex. position=1; [1,2,3] -> 1.
 
-        if max_value == second_value:
-            return (-1, -1)
-        return index_storage[second_value], second_value
-    except (AssertionError, KeyError):
-        # In case there is no integer values or incorrect input type
+
+        Summary: This function will filter type int input values and return
+                 the original index and value from the desired position. The
+                 range is from max to min int values, ex. [10,9,8,7,6,5,4...].
+
+        Return: (index, value) or (-1,1) if not found.
+    '''
+    position -= 1
+    sort_list = list((index, value) for index, value in enumerate(input)
+                     if isinstance(value, int))
+    sort_list.sort(key=lambda enum_obj: enum_obj[1], reverse=True)
+    try:
+        s_index, s_value = sort_list[position][0], sort_list[position][1]
+        return s_index, s_value
+    except IndexError:
         return (-1, -1)
 
 
 if __name__ == "__main__":
-    print(sortsecondLargest([2, 1, 4, 5, 6, 7, 8, 's', 's', 's', 'uj', 'sdd',
-                            223, 'e', 1000, 'g', 2000, 'v', 'v', 'l',
-                            900000000]))
-    print(sortsecondLargest(['a', 'b']))
-    print(sortsecondLargest(['a', 'b', 2]))
-    print(sortsecondLargest(['a', 'b', 2, 3, 4, 6, 100, 80, 80, 80]))
-
+    print(sortnLargest([2, 1, 4, 5, 6, 7, 8, 's', 's', 's', 'uj', 'sdd',
+                        223, 'e', 1000, 'g', 2000, 'v', 'v', 'l',
+                        900000000], 2))
+    print(sortnLargest(['a', 'b'], 2))
+    print(sortnLargest(['a', 'b', 2], 2))
+    print(sortnLargest(['a', 'b', 2, 3, 4, 6, 100, 80, 80, 80], 2))
 
 # Example Input
-    print(sortsecondLargest(['a', 'b', 1, 4, 5]))
+    print(sortnLargest(['a', 'b', 1, 4, 5], 2))
